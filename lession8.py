34aab32ab40236d4cfb2a3371e18af22558bc98e
tor <tor.berglund@gmil.com>
2016-03-15 23:22:36 +0100

    test

--- lession8.py
+++ lession8.py
@@ -13,14 +13,14 @@ p.start(0)  # Start PWM output, Duty Cycle = 0
 
 try:
     while True:
-        for dc in range(0, 101, 4):  # Increase duty cycle: 0~100
+        for dc in range(0, 101, 1):  # Increase duty cycle: 0~100
             p.ChangeDutyCycle(dc)  # Change duty cycle
-            time.sleep(0.05)
+            time.sleep(0.01)
         time.sleep(1)
-        for dc in range(100, -1, -4):  # Decrease duty cycle: 100~0
+        for dc in range(100, -1, -1):  # Decrease duty cycle: 100~0
             p.ChangeDutyCycle(dc)
-            time.sleep(0.05)
+            time.sleep(0.01)
 except KeyboardInterrupt:
     p.stop()
     GPIO.output(LedPin, GPIO.HIGH)  # turn off all leds
-    GPIO.cleanup() 
+    GPIO.cleanup()
